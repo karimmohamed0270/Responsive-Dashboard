45f952b257645eb453421295b2caf435ac8da761
Karim Mohamed <karimmohamed0270@gmail.com>
2026-03-27 19:12:40 +0200

    create my card page view widget

---
+++ lib/widgets/my_card_page_view.dart
@@ -0,0 +1,12 @@
+import 'package:expandable_page_view/expandable_page_view.dart';
+import 'package:flutter/material.dart';
+import 'package:responesive_dashboard/widgets/my_card.dart';
+
+class MyCardPageView extends StatelessWidget {
+  const MyCardPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpandablePageView(children: List.generate(3, (index) => MyCard()));
+  }
+}
